571c479e1c27d7ad37dd9b3306c6890b859d6085
Piyush Sultaniya <87389164+gitPiyushhh@users.noreply.github.com>
2025-02-01 12:44:56 +0530

    Minor changes

--- controllers/assets.js
+++ controllers/assets.js
@@ -13,9 +13,20 @@ const s3 = new AWS.S3({
 
 // Set up Multer for file uploads
 const storage = multer.memoryStorage();
+// const upload = multer({
+//   storage,
+//   limits: { fileSize: 15 * 1024 * 1024 }, // 10 MB file size limit
+// }).single('image');
 const upload = multer({
   storage,
-  limits: { fileSize: 15 * 1024 * 1024 }, // 10 MB file size limit
+  limits: { fileSize: 15 * 1024 * 1024 }, // 15 MB file size limit
+  fileFilter: (req, file, cb) => {
+    const allowedTypes = ['image/jpeg', 'image/png', 'image/gif', 'image/heic'];
+    if (!allowedTypes.includes(file.mimetype)) {
+      return cb(new AppError('Invalid file type', 400), false);
+    }
+    cb(null, true);
+  },
 }).single('image');
 
 exports.uploadImage = catchAsync(async (req, res, next) => {
